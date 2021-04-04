d5427e1a7dcb8104f5453d7bb1a987086b486f19
Mahmoud Al-Dinah <mahmood.dinah2@gmail.com>
2021-04-04 14:27:19 +0300

    new

---
+++ js/app.js
@@ -0,0 +1,16 @@
+"user strict"
+let userName = prompt("what's your name?");
+
+console.log(userName);
+
+let userAge = prompt("how old are you");
+console.log(userAge);
+
+let favEquip = prompt("whats your favourite equipment?" , 'Hammer , Gloves , Helmet');
+console.log(favEquip);
+
+let favSeas = prompt('Which season do you like?' , 'Summer , winter');
+console.log(favSeas);
+
+
+alert('Your name is : ' + userName + '  and your age is : ' + userAge + '  your favourite equipment is : ' + favEquip + '  your favourite season is : ' + favSeas);
