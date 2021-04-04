d7f0d36c609004a9663a3b20612a6812f8e969d9
Mahmoud Al-Dinah <mahmood.dinah2@gmail.com>
2021-04-04 14:29:05 +0300

    added typeoff

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 "user strict"
 let userName = prompt("what's your name?");
 
-console.log(userName);
+console.log(typeof userName);
 
 let userAge = prompt("how old are you");
-console.log(userAge);
+console.log(typeof userAge);
 
 let favEquip = prompt("whats your favourite equipment?" , 'Hammer , Gloves , Helmet');
-console.log(favEquip);
+console.log(typeof favEquip);
 
 let favSeas = prompt('Which season do you like?' , 'Summer , winter');
-console.log(favSeas);
+console.log(typeof favSeas);
 
 
 alert('Your name is : ' + userName + '  and your age is : ' + userAge + '  your favourite equipment is : ' + favEquip + '  your favourite season is : ' + favSeas);
